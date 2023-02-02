cf65992c04a9add46356aff657f7906c44c60644
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-02 14:03:11 +0100

    css + js - added red boxes

--- js/script.js
+++ js/script.js
@@ -12,18 +12,16 @@ const levelThree = 49;
  *               FUNCTIONS                  *
  *                                          *
  ********************************************/
-
 /**
- * funzione per generare una griglia dinamica a seconda dei diversi livelli
- * @param {HTMLElement} grid l'elemento nel quale inserire i box
- * @param {Int} dimension la dimensione della griglia (in base al livello)
+ * funzione per generare una griglia dinamica a seconda dei diversi livelli e per generare 16 bombe casuali nel range di box;
+ * @param {HTMLElement} grid l'elemento nel quale inserire i box;
+ * @param {Int} level livello scelto dall'utente e dimensione griglia;
  */
-
-function generateGrid(grid, dimension) { //Livello 1
+function generateGrid(grid, level) { //Livello 1
     // Parti da un luogo sempre vuoto
     grid.innerHTML = "";
     // Per n volte
-    for (let i = 1; i <= dimension; i++) {
+    for (let i = 1; i <= level; i++) {
         // Crea un div e assegna loro una classe (dimensioni e bordo)
         const boxElements = document.createElement("div");
 
@@ -48,23 +46,25 @@ function generateGrid(grid, dimension) { //Livello 1
             "click",
             function () {
                 console.log(this);
+                let boxclicked = this;
                 this.classList.toggle("cell-bg");
                 // Stampali in console
                 console.log("Il box che hai cliccato è il numero: " + boxCell);
+
+                if (placeBombs.includes(boxCell)) {
+                    this.classList.toggle("cell-warning");
+                }
+
             }
         )
 
     }
-}
-/**
- * funzione per generare 16 bombe casuali nel range di box possibili a seconda dei livelli
- * @param {value} level il livello scelto dall'utente
- */
-function generateBombs(level) {
+
     // INSERISCO UN CICLO PER GENERARE NUMERI RANDOM (in base al livello selezionato dall'utente)
     let placeBombs = [];
     while (placeBombs.length < 16) {
-        let randomBombNumber = Math.floor(Math.random() * level - 1) + 1;
+        // Math.random inclusive formula
+        let randomBombNumber = Math.floor(Math.random() * (level - 1 + 1) + 1);
 
         if (!placeBombs.includes(randomBombNumber)) {
             placeBombs.push(randomBombNumber);
@@ -73,12 +73,14 @@ function generateBombs(level) {
     }
     return placeBombs;
 }
+
+
 /*************************************
  *                                   *
  *             ON CLICK              *
  *************************************/
 
-// Al click del bottone, a seconda della scelta del giocatore - crea la griglia
+// Al click del bottone, a seconda della scelta del giocatore - crea la griglia- genera la bombe
 submitChoiceButton.addEventListener(
     "click",
     function () {
@@ -86,7 +88,7 @@ submitChoiceButton.addEventListener(
 
         if (levelUserSelection == "level-1") {
             generateGrid(containerGrid, levelOne);
-            generateBombs(levelOne);
+
         } if (levelUserSelection == "level-2") {
             generateGrid(containerGrid, levelTwo);
             generateBombs(levelTwo);
